54e155d249109747bbe2f1d49e6cf23fd99fa95d
Juan Jose Sáenz <juansamo2007@gmail.com>
2026-06-03 14:40:58 -0500

    Delete fixapp.ps1

--- fixapp.ps1
+++
@@ -1,3 +0,0 @@
-$c = Get-Content 'app.js' -Encoding UTF8
-$c = $c -replace 'Ã“','Ó' -replace 'Ã³','ó' -replace 'Ã¡','á' -replace 'Ã©','é' -replace 'Ã­','í' -replace 'Ã±','ñ' -replace 'Ãº','ú' -replace 'âœ…','✓' -replace 'âŒ','❌' -replace 'âš ï¸','⚠️' -replace 'â„¹ï¸','ℹ️' -replace 'âœ”','”' -replace 'âœ‘','‘' -replace 'Â¿','¿' -replace 'Â¡','¡'
-Set-Content 'app.js' -Value $c -Encoding UTF8
